833a38ff9e818c356a52d66adb2176a12c9ef7dd
ITLimJiaWei <73027268+ITLimJiaWei@users.noreply.github.com>
2021-04-06 22:16:01 +0800

    Added js for searchCountry

--- script.js
+++ script.js
@@ -1,80 +1,113 @@
-var settings = {
+
+$(document).ready(function () {
+  var settings = {
     "url": "https://api.covid19api.com/summary",
     "method": "GET",
     "timeout": 0,
   };
   
-  $.ajax(settings).done(function (response) {                                     //Retrieving Global Data
-    console.log(response.Global.TotalConfirmed);   //TESTING (TO BE REMOVED)
-    
+  $.ajax(settings).done(function (response) {                                     //Retrieving Global Data  
     $("#total-cases-value").append(response.Global.TotalConfirmed);
-    $("#new-cases-value").append(response.Global.NewConfirmed);
+    $("#new-cases-value").append("+"+response.Global.NewConfirmed);
     $("#death-value").append(response.Global.TotalDeaths);
     $("#total-recovered-value").append(response.Global.TotalRecovered);
-    $("#new-recovered-value").append(response.Global.NewRecovered);
-    $("#new-death-value").append(response.Global.NewDeaths);
+    $("#new-recovered-value").append("+"+response.Global.NewRecovered);
+    $("#new-death-value").append("+"+response.Global.NewDeaths);
   });
 
 
-  var country1 = {
+  var malaysia = {
     "url": "https://api.covid19api.com/live/country/malaysia",           //Retrieving Malaysia Data
     "method": "GET",
     "timeout": 0,
   };
   
-  $.ajax(country1).done(function (response) {
+  $.ajax(malaysia).done(function (response) {
     console.log(response);                        //TESTING (TO BE REMOVED)
     $("#recovered-cases-malaysia").append(response[response.length-1].Recovered);
-    // $("#total-cases-malaysia").append(response[response.length-1].Confirmed);
     $("#total-deaths-malaysia").append(response[response.length-1].Deaths);
     var active_cases = response[response.length-1].Confirmed-response[response.length-1].Recovered;
-    var new_cases = response[response.length-1].Confirmed-response[response.length-2].Confirmed;     //IMPLEMENT NEW CASES STATISTIC
-    console.log(new_cases);
+    var new_cases = response[response.length-1].Confirmed-response[response.length-2].Confirmed;     
+    $("#confirmed-cases-malaysia").append(response[response.length-1].Confirmed)
     $("#active-cases-malaysia").append(active_cases);
-    $("#new-cases-malaysia").append(new_cases);
+    $("#new-cases-malaysia").append("+"+new_cases);
   }); 
 
-  var country2 = {
+  var singapore = {
     "url": "https://api.covid19api.com/live/country/singapore",           //Retrieving Singapore Data
     "method": "GET",
     "timeout": 0,
   };
   
-  $.ajax(country2).done(function (response) {
+  $.ajax(singapore).done(function (response) {
     console.log(response);                        //TESTING (TO BE REMOVED)
     $("#recovered-cases-singapore").append(response[response.length-1].Recovered);
     $("#total-cases-singapore").append(response[response.length-1].Confirmed);
-    $("#total-deaths-singapore").append(response[response.length-1].Deaths);
     var active_cases = response[response.length-1].Confirmed-response[response.length-1].Recovered;
+    var new_cases = response[response.length-1].Confirmed-response[response.length-2].Confirmed; 
+    $("#new-cases-singapore").append("+"+new_cases);
+    $("#total-deaths-singapore").append(response[response.length-1].Deaths);
     $("#active-cases-singapore").append(active_cases);
   });
 
-  var country3 = {
+  var indonesia = {
     "url": "https://api.covid19api.com/live/country/indonesia",           //Retrieving Indonesia Data
     "method": "GET",
     "timeout": 0,
   };
   
-  $.ajax(country3).done(function (response) {
+  $.ajax(indonesia).done(function (response) {
     console.log(response);                        //TESTING (TO BE REMOVED)
     $("#recovered-cases-indonesia").append(response[response.length-1].Recovered);
     $("#total-cases-indonesia").append(response[response.length-1].Confirmed);
     $("#total-deaths-indonesia").append(response[response.length-1].Deaths);
     var active_cases = response[response.length-1].Confirmed-response[response.length-1].Recovered;
+    var new_cases = response[response.length-1].Confirmed-response[response.length-2].Confirmed; 
+    $("#new-cases-indonesia").append("+"+new_cases);
     $("#active-cases-indonesia").append(active_cases);
   });
 
-  var country4 = {
+  var israel = {
     "url": "https://api.covid19api.com/live/country/israel",           //Retrieving Israel Data
     "method": "GET",
     "timeout": 0,
   };
   
-  $.ajax(country4).done(function (response) {
+  $.ajax(israel).done(function (response) {
     console.log(response);                        //TESTING (TO BE REMOVED)
     $("#recovered-cases-israel").append(response[response.length-1].Recovered);
     $("#total-cases-israel").append(response[response.length-1].Confirmed);
     $("#total-deaths-israel").append(response[response.length-1].Deaths);
     var active_cases = response[response.length-1].Confirmed-response[response.length-1].Recovered;
+    var new_cases = response[response.length-1].Confirmed-response[response.length-2].Confirmed; 
+    $("#new-cases-israel").append("+"+new_cases);
     $("#active-cases-israel").append(active_cases);
   });
+
+  function countrySearch(e) {
+    e.preventDefault();
+    var countryName = $("#country-input").val();
+    console.log(countryName)                    //TESTING (TO BE REMOVED)
+    var countrySearch = {
+      "url": "https://api.covid19api.com/live/country/" + countryName,           //Retrieving Israel Data
+      "method": "GET",
+      "timeout": 0,
+    };
+    
+    $.ajax(countrySearch).done(function (response) {
+      console.log(response[response.length-1].Recovered);  //TESTING (TO BE REMOVED)   
+      $("#country-search-name").empty();                 //Note (Add loading animation)
+      $("#country-search-name").append(response[response.length-1].Country.toUpperCase())     
+      $("#country-search-flag").empty();
+      $("#country-search-flag").append("<img src='https://www.countryflags.io/"+ response[0].CountryCode.toLowerCase()  + "/flat/64.png'>")
+    });
+
+
+  }
+
+  const form = document.getElementById('search-form');
+  form.addEventListener('submit',countrySearch)
+
+});
+
+
